e0a03555a2d55384578d137dca45c273614ac12b
steven <steven_ko@paradise-soft.com.tw>
2022-06-28 16:37:58 +0800

    feat: add Post component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,43 @@ import IGHeader from "components/IGHeader"
 import IGContainer from "components/IGContainer"
 import IGStory from "./components/IGStory"
 import IGUser from 'components/IGUser'
+import IGPost from "./components/IGPost"
+import db from '../../db.json'
+
+const IGPostList: React.FC = () => {
+  const data = db.posts
+  return (
+    <>
+      {
+        data?.map((item) => {
+          const {
+            id,
+            location,
+            account,
+            avatar,
+            photo,
+            likes,
+            description,
+            hashTags,
+            createTime,
+          } = item;
+          return (
+            <IGPost
+              location={location}
+              account={account}
+              avatar={avatar}
+              photo={photo}
+              likes={likes}
+              description={description}
+              hashTags={hashTags}
+              createTime={createTime}
+              key={id}
+            />
+          );
+        })}
+    </>
+  )
+}
 
 const Home: React.FC = () => {
   return (
@@ -11,6 +48,7 @@ const Home: React.FC = () => {
         <div className="flex lg: justify-center">
           <div className="w-full lg:w-[600px]">
             <IGStory />
+            <IGPostList />
           </div>
           <div className="hidden lg:block lg:w-[424px] ">
             <IGUser avatar="/images/avatars/a1.png" account="bruce_1234" location="布魯斯前端" showFollow={true} />
